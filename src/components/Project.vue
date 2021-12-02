2a455351eae05cdb9d6a3d7cc99068038a52bc60
Lin <allenlin@diamondonline.com.tw>
2021-12-02 18:00:10 +0800

    project notyet

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,43 +1,133 @@
 <template>
-  <div class="main"></div>
+  <div class="main">
+    <h1>PROJECT</h1>
+    <hr />
+    <div class="project">
+      <div class="project-item" v-for="item in Project_data" :key="item">
+        <div class="img" :class="item.name" @click="goto(item.goto)">
+          <img :src="item.img" alt="" />
+          <div class="name">
+            <p>{{ item.name }}</p>
+          </div>
+        </div>
+        <p>{{ item.content }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "Project",
   data() {
     return {
-      data: [
+      Project_data: [
         {
-          name: "偵茶者",
-          content: "",
-          img: require("../assets/logo.png"),
+          name: "Diamond-Online 官網",
+          content: "Angular",
+          img: require("../assets/Diamondweb.png"),
+          goto: "https://www.diamondonline.com.tw/official-website",
+        },
+        {
+          name: "Diamond-Online 後台",
+          content: "JavaScript jQuery Ajax C# ASP.NET",
+          img: require("../assets/Diamondback.png"),
+          goto: "",
+        },
+        {
+          name: "Diamond-Online 實體店鋪控管平台",
+          content: "Vue.js Vue-cli Vuex Vue-router axios",
+          img: require("../assets/Diamondrest.png"),
           goto: "https://tibamef2e.com/ced101/project/g2/index.html",
         },
         {
           name: "偵茶者",
-          content: "",
-          img: require("../assets/logo.png"),
-          goto: "https://tibamef2e.com/ced101/project/g2/index.html",
-        },
-        {
-          name: "偵茶者",
-          content: "",
+          content: "Vue.js jQuery Gulp PHP",
           img: require("../assets/logo.png"),
           goto: "https://tibamef2e.com/ced101/project/g2/index.html",
         },
         {
           name: "ZOEY Dance studio",
-          content: "",
+          content: "JavaScript jQuery",
           img: require("../assets/logo.png"),
           goto: "https://chiehlin23.github.io/works/",
         },
       ],
     };
   },
+  methods: {
+    goto(url) {
+      window.open(url);
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.main {
+  background: url("../assets/background.jpg");
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+  padding: 8rem;
+}
+.project {
+  display: flex;
+  /* border: 1px solid black; */
+  width: 50%;
+  margin: 0 auto;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.project-item {
+  margin: 1rem;
+  width: 13rem;
+}
+.img {
+  position: relative;
+  height: 200px;
+  overflow: hidden;
+  cursor: pointer;
+  border-radius: 1rem;
+  box-shadow: 3px 3px 3px black;
+}
+.img .name {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  background-color: rgb(0, 0, 0, 0.5);
+  display: none;
+}
+.img .name p {
+  margin: 0;
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.img:hover .name {
+  display: block;
+}
+
+h1 {
+  margin: 0;
+  font-size: 3rem;
+  color: #fafafe;
+  letter-spacing: 0.1rem;
+}
+p {
+  color: #fafafe;
+  line-height: 1.5rem;
+  letter-spacing: 0.1rem;
+}
+hr {
+  margin: 2rem auto;
+  height: 5px;
+  background-color: #fafafe;
+  width: 50px;
+}
 </style>
