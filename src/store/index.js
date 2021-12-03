a017d4e3f20b8f8040bff52cbc6e10ceb8cee629
Lin <allenlin@diamondonline.com.tw>
2021-12-03 10:13:30 +0800

    20211203_1013

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    project: '0'
   },
   mutations: {
+    projectOpen(state, value) {
+      state.project = value;
+    }
   },
   actions: {
   },
